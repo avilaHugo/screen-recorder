214d960366144003133566adcb19df7573b538a9
Hugo L. Ávila <53014804+avilaHugo@users.noreply.github.com>
2024-01-23 08:35:31 -0300

    adding ultrafast encoding.

--- webcan_rec.sh
+++ webcan_rec.sh
@@ -10,6 +10,6 @@ OUTPUT_NAME="${1?'No output name was passed !'}"
 ffmpeg \
     -y \
     -f v4l2 -input_format mjpeg -framerate 30 -video_size '640x480' -thread_queue_size 1024 -i /dev/video0 \
-    -c:v libx264 \
+    -c:v libx264 -preset ultrafast \
     "${OUTPUT_NAME}"
 
