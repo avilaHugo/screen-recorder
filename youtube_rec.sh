e805bce00c180c5847c5d80b56ebd8a3c7b4dd9e
Hugo L. Ávila <53014804+avilaHugo@users.noreply.github.com>
2024-01-25 18:18:00 -0300

    Remove single scripts. Change settings.

--- youtube_rec.sh
+++ youtube_rec.sh
@@ -5,24 +5,48 @@ OUTPUT_PREFIX="${1?'No OUTPUT_PREFIX was passed !'}-${NOW}"
 DIRNAME=$(dirname ${OUTPUT_PREFIX} )
 STOP_SIGN="$( mktemp -p ${DIRNAME} -t STOP_SIGN.XXXXXX )"
 
-sleep 3 && < "${STOP_SIGN}" screen_rec.sh "${OUTPUT_PREFIX}.mkv" &>/dev/null &
-sleep 3 && < "${STOP_SIGN}" webcan_rec.sh "${OUTPUT_PREFIX}.mp4" &>/dev/null &
+
+# This configs work for my setup, check the commands before
+# attempting to use it.
+
+# Grab screen (no audio)
+< "${STOP_SIGN}" ffmpeg \
+    -f x11grab -video_size '1920x1080' -framerate 30 -thread_queue_size 1024 -i "${DISPLAY}" \
+    -c:v libx264rgb -crf 0 -color_range 2 \
+    -preset ultrafast \
+    "${OUTPUT_PREFIX}.screen.mp4" &>/dev/null &
+
+# Record webcan feed with its audio 
+< "${STOP_SIGN}" ffmpeg \
+    -y \
+    -f alsa  -ac 2 -ar 48000  -i 'default' \
+    -f v4l2 -input_format mjpeg -framerate 30 -video_size '640x480' -thread_queue_size 1024 -i /dev/video0 \
+    -b:a 256k -c:a pcm_s16le \
+    -c:v libx264  \
+    -preset ultrafast  \
+    "${OUTPUT_PREFIX}.webcan.mp4" &>/dev/null &
 
 while :; do
-    read -p 'Press [q] To stop recording ...' Q
-    [[ ${Q} == 'q' ]] && {
-        echo "${Q}" > "${STOP_SIGN}"
-        rm -v "${STOP_SIGN}"
-        exit 0
+    # Here we are prompting the user for the stop sign (the letter q).
+    read -p 'Press [q] To stop recording ... ' Q
+
+    [[  ${Q} != 'q' ]] && {
+        echo "You did not press [q], received: ${Q}"
+        continue
     }
-    echo "You did not press [q]: ${Q}"
+
+    # ffmepg can read the 'q' from a file passed on the stdin
+    # so here we are writting to the STOP_SIGN file to signal
+    # the ffmpeg instances to stop gracefully. This is VERY IMPORTANT !!!
+    # Althought newer versions say that you can scape it with Ctrl+C
+    # doing so might lead to a broken file.
+    echo "${Q}" > "${STOP_SIGN}"
+
+    # Let's wait the instances finish.
+    wait
+
+    #+TODO (hugo.avila): Add a trap for this.
+    rm -v "${STOP_SIGN}"
+
+    exit 0
 done
-  
-
-
-
-
-
-
-
-
